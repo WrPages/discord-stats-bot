5c345dced02c2bde0ca05f8466def4aaf7e07f01
WrPages <wenderramirez00@gmail.com>
2026-03-30 00:50:10 -0600

    Actualizar config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,12 @@ module.exports = {
   token: process.env.LATIOS_TOKEN,
   clientId: process.env.LATIOS_ID,
   guildId: process.env.GUILD_ID,
-  statsUrl: process.env.STATS_URL
 
-  // 📊 En código (tus 3 variables)
-  statsUrl: "https://gist.githubusercontent.com/WrPages/bb18eda2ea748723d8fe0131dd740b70/raw/elite_users.json",      // registro de usuarios
-  onlineUrl: "https://gist.githubusercontent.com/WrPages/d9db3a72fed74c496fd6cc830f9ca6e9/raw/elite_ids.txt",       // lista de IDs online
-  heartbeatChannelId: "1483616146996465735"  // canal donde llegan stats
-panelChannelId: "1484015417411244082"
+  // 📊 Gists
+  statsUrl: "https://gist.githubusercontent.com/WrPages/bb18eda2ea748723d8fe0131dd740b70/raw/elite_users.json",
+  onlineUrl: "https://gist.githubusercontent.com/WrPages/d9db3a72fed74c496fd6cc830f9ca6e9/raw/elite_ids.txt",
+
+  // 💬 Canales
+  heartbeatChannelId: "1483616146996465735",
+  panelChannelId: "1484015417411244082"
 };
