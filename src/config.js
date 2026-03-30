8e1c639ac6c7f2b4142cd7767db56498253a1cc9
WrPages <wenderramirez00@gmail.com>
2026-03-30 00:43:53 -0600

    Actualizar config.js

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ module.exports = {
   statsUrl: "https://gist.githubusercontent.com/WrPages/bb18eda2ea748723d8fe0131dd740b70/raw/elite_users.json",      // registro de usuarios
   onlineUrl: "https://gist.githubusercontent.com/WrPages/d9db3a72fed74c496fd6cc830f9ca6e9/raw/elite_ids.txt",       // lista de IDs online
   heartbeatChannelId: "1483616146996465735"  // canal donde llegan stats
-
+panelChannelId: "AQUI_CANAL_PANEL"
 };
