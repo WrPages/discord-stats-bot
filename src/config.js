11e3ca5816d7526b4ebb5794c07f138a6336a8eb
WrPages <wenderramirez00@gmail.com>
2026-03-30 00:42:16 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ module.exports = {
   statsUrl: process.env.STATS_URL
 
   // 📊 En código (tus 3 variables)
-  statsUrl: "AQUI_GIST_USUARIOS",      // registro de usuarios
-  onlineUrl: "AQUI_GIST_ONLINE",       // lista de IDs online
-  heartbeatChannelId: "AQUI_CANAL_ID"  // canal donde llegan stats
+  statsUrl: "https://gist.githubusercontent.com/WrPages/bb18eda2ea748723d8fe0131dd740b70/raw/elite_users.json",      // registro de usuarios
+  onlineUrl: "https://gist.githubusercontent.com/WrPages/d9db3a72fed74c496fd6cc830f9ca6e9/raw/elite_ids.txt",       // lista de IDs online
+  heartbeatChannelId: "1483616146996465735"  // canal donde llegan stats
 
 };
