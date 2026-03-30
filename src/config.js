4386f0d811d4ea8a23ff5c9da5bb46e9df05de7e
WrPages <wenderramirez00@gmail.com>
2026-03-30 00:35:04 -0600

    Actualizar config.js

--- src/config.js
+++ src/config.js
@@ -3,4 +3,10 @@ module.exports = {
   clientId: process.env.LATIOS_ID,
   guildId: process.env.GUILD_ID,
   statsUrl: process.env.STATS_URL
+
+  // 📊 En código (tus 3 variables)
+  statsUrl: "AQUI_GIST_USUARIOS",      // registro de usuarios
+  onlineUrl: "AQUI_GIST_ONLINE",       // lista de IDs online
+  heartbeatChannelId: "AQUI_CANAL_ID"  // canal donde llegan stats
+
 };
