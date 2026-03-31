bcb321d56a8c8211d3dd1f5a770fa309c79399b3
WrPages <wenderramirez00@gmail.com>
2026-03-31 00:08:55 -0600

    Actualizar bot.js

--- src/bot.js
+++ src/bot.js
@@ -277,7 +277,7 @@ const col = (text, width = 10) => {
 const colTitle = (text) => col(text, 11);
 const colValue = (text) => col(text, 12); // 👈 más ancho para centrar números
 
-function pad(value, width = 8.5) {
+function pad(value, width = 9) {
   return String(value).padStart(width, " ");
 }
 
