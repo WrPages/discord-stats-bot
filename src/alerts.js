6fa0c4dcf2b828b231ba624a9c3581ca7f42c67d
WrPages <wenderramirez00@gmail.com>
2026-03-31 20:28:50 -0600

    Update alerts.js

--- src/alerts.js
+++ src/alerts.js
@@ -1,13 +1,21 @@
+
 const { 
     ChannelType, 
     PermissionFlagsBits 
 } = require('discord.js');
 
-const registeredUsers = require('../elite_users.json');
-
 const HEARTBEAT_CHANNEL_ID = '1483616146996465735';
 const CATEGORY_ID = '1488253270068691045'; // opcional
 const CHAMPION_ROLE_ID = '1486206362332434634';
+
+// 🔥 PON AQUÍ EL RAW URL DE TU GIST
+const GIST_URL = 'https://gist.githubusercontent.com/WrPages/bb18eda2ea748723d8fe0131dd740b70/raw/elite_users.json';
+
+async function loadUsers() {
+    const response = await fetch(GIST_URL);
+    if (!response.ok) throw new Error('No se pudo cargar el Gist');
+    return await response.json();
+}
 
 module.exports = (client) => {
 
@@ -18,10 +26,11 @@ module.exports = (client) => {
             if (message.author.bot) return;
             if (message.channel.id !== HEARTBEAT_CHANNEL_ID) return;
 
-            // Primera línea = nombre del reroller
+            // Cargar usuarios dinámicamente desde Gist
+            const registeredUsers = await loadUsers();
+
             const firstLine = message.content.split('\n')[0].trim();
 
-            // Buscar en tu JSON
             const entry = Object.entries(registeredUsers)
                 .find(([discordId, data]) => data.name === firstLine);
 
@@ -36,9 +45,7 @@ module.exports = (client) => {
                 c => c.name === channelName
             );
 
-            // Crear canal si no existe
             if (!userChannel) {
-
                 userChannel = await guild.channels.create({
                     name: channelName,
                     type: ChannelType.GuildText,
@@ -65,8 +72,6 @@ module.exports = (client) => {
                         },
                     ],
                 });
-
-                console.log(`Canal creado para ${userData.name}`);
             }
 
             await userChannel.send(
