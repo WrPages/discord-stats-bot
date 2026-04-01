f8eb0c8ce97204d03171372799dbf72433a2769c
WrPages <wenderramirez00@gmail.com>
2026-03-31 18:25:55 -0600

    Update alerts.js

--- src/alerts.js
+++ src/alerts.js
@@ -1,1 +1,15 @@
+// src/alerts.js
 
+module.exports = (client) => {
+    client.on('ready', () => {
+        console.log('Sistema de alertas cargado correctamente');
+    });
+
+    // Ejemplo: alerta cuando alguien entra
+    client.on('guildMemberAdd', member => {
+        const channel = member.guild.systemChannel;
+        if (channel) {
+            channel.send(`Bienvenido ${member.user.username} al servidor! 🎉`);
+        }
+    });
+};
